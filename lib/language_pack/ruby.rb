d4b6b0583410e75258879619a48eead506eb3393
Ariel Schvartz <ari.shh@gmail.com>
2018-07-27 12:05:23 -0300

    Trying to make it work

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -657,9 +657,37 @@ WARNING
           puts "Running: #{bundle_command}"
           instrument "ruby.bundle_install" do
             bundle_time = Benchmark.realtime do
+
+              puts "\n\nStarting SQLite3 installation\n\n"
+              cmd1 = "export GEM_PATH=#{ENV["GEM_PATH"]};"
+              cmd2 = "export GEM_HOME=#{ENV["GEM_HOME"]};"
+              cmd_sqlite3_install = File.read("#{File.dirname(__FILE__)}/../../support/sqlite3/install.sh")
+
+              cmds = "#{cmd1} #{cmd2} #{cmd_sqlite3_install}"
+              bundler_output << pipe("#{cmds}")
+              puts "\n\nFinish SQLite3 installation\n\n"
+
               bundler_output << pipe("#{bundle_command} --no-clean", out: "2>&1", env: env_vars, user_env: true)
             end
           end
+
+          # puts "Running: #{bundle_command}"
+          # instrument "ruby.bundle_install" do
+          #   bundle_time = Benchmark.realtime do
+          #     # Bikle
+          #       hbs = "#{pwd}/heroku_buildpack_scripts"
+          #       # If #{hbs}/sqlite_hack.bash exists, then run it.
+          #       if File.exists?("#{hbs}/sqlite_hack.bash")
+          #         cmd1 = "export GEM_PATH=#{ENV["GEM_PATH"]};"
+          #         cmd2 = "export GEM_HOME=#{ENV["GEM_HOME"]};"
+          #         cmd3 = "/bin/bash #{hbs}/sqlite_hack.bash"
+          #         cmds = "#{cmd1} #{cmd2} #{cmd3} "
+          #         bundler_output << pipe("cmds")
+          #       end
+          #     # Bikle
+          #     bundler_output << pipe("#{bundle_command} --no-clean", out: "2>&1", env: env_vars, user_env: true)
+          #   end
+          # end
         end
 
         if $?.success?
---
+++ support/sqlite3/install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# I use this script to help me enhance the ability of 
+# heroku-buildpack
+
+rails_root=`pwd`
+
+echo rails_root is
+echo $rails_root
+cd ${rails_root}/vendor/
+
+# I should install sqlite3 software locally under Rails.root
+
+mkdir -p ${rails_root}/vendor/sqlite3
+mkdir -p ${rails_root}/vendor/gems/sqlite3
+
+tar zxf  ${rails_root}/vendor/sqlite-autoconf-3230100.tar.gz 
+cd sqlite-autoconf-3230100/
+./configure --prefix=${rails_root}/vendor/sqlite3
+make
+make install
+
+cd ${rails_root}/vendor/
+
+# I should be able to gem install sqlite3 now
+gem install -v 1.3.13 --install-dir gems/sqlite3/ sqlite3 -- --with-sqlite3-dir=${rails_root}/vendor/sqlite3
+cp gems/sqlite3/specifications/sqlite3-1.3.13.gemspec gems/sqlite3/gems/sqlite3-1.3.13/
+
+exit

75a30349fd413080a0ee2b4d36c71fb45ad2747f
Ariel Schvartz <ari.shh@gmail.com>
2018-07-27 12:24:44 -0300

    Fix

--- support/sqlite3/install.sh
+++ support/sqlite3/install.sh
@@ -21,9 +21,9 @@ echo Startind download...
 wget https://www.sqlite.org/2018/sqlite-autoconf-3230100.tar.gz
 echo Download finished.
 
-tar zxf sqlite-autoconf-3230100.tar.gz
+tar zxf ${rails_root}/sqlite-autoconf-3230100.tar.gz
 
-cd sqlite-autoconf-3230100/
+cd ${rails_root}/sqlite-autoconf-3230100/
 ./configure --prefix=${rails_root}/vendor/sqlite3
 make
 make install
@@ -31,7 +31,6 @@ make install
 cd ${rails_root}/vendor/
 
 # I should be able to gem install sqlite3 now
-gem install -v 1.3.13 --install-dir gems/sqlite3/ sqlite3 -- --with-sqlite3-dir=${rails_root}/vendor/sqlite3
-cp gems/sqlite3/specifications/sqlite3-1.3.13.gemspec gems/sqlite3/gems/sqlite3-1.3.13/
+gem install -v 1.3.13 -- --with-sqlite3-dir=${rails_root}/vendor/sqlite3
 
 exit
